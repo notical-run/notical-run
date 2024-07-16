76f80cd0edc2ad93e627fa67ae3c0a560c84e8b7
Akshay Nair <phenax5@gmail.com>
2024-07-16 12:11:18 +0530

    Prevent duplicate node ids. Handles case for copy-pasted code blocks

--- apps/notes-app/src/components/Editor/extensions/GlobalNodeId.ts
+++ apps/notes-app/src/components/Editor/extensions/GlobalNodeId.ts
@@ -27,22 +27,27 @@ export const GlobalNodeId = Node.create({
       new Plugin({
         appendTransaction(_transactions, oldState, newState) {
           if (newState.doc === oldState.doc) return;
+          // Track ids in use to overwrite duplicate ids
+          const inUseUuids = new Set();
 
           const { tr } = newState;
           newState.doc.descendants((node, pos, _parent) => {
-            if (nodeTypes.has(node.type.name) && !node.attrs.nodeId) {
-              tr.setNodeAttribute(pos, 'nodeId', `${crypto.randomUUID()}`);
+            const isIDDuplicated = inUseUuids.has(node.attrs.nodeId);
+            if (nodeTypes.has(node.type.name) && (isIDDuplicated || !node.attrs.nodeId)) {
+              const uuid = crypto.randomUUID();
+              tr.setNodeAttribute(pos, 'nodeId', `${uuid}`);
             } else if (node.isText) {
               const nodeMark = node.marks.find(m => markTypes.has(m.type.name));
-              if (nodeMark && !nodeMark.attrs.nodeId) {
+              if (nodeMark && (isIDDuplicated || !nodeMark.attrs.nodeId)) {
+                const uuid = crypto.randomUUID();
+                inUseUuids.add(uuid);
                 nodeMark.removeFromSet(node.marks);
-                (nodeMark as any).attrs = {
-                  ...nodeMark.attrs,
-                  nodeId: `${crypto.randomUUID()}`,
-                };
+                (nodeMark as any).attrs = { ...nodeMark.attrs, nodeId: `${uuid}` };
                 nodeMark.addToSet(node.marks);
               }
             }
+
+            inUseUuids.add(node.attrs.nodeId);
           });
 
           return tr;
