e2a28655707e16f84b9dd3cf02527db8683eec2e
Akshay Nair <phenax5@gmail.com>
2024-09-04 22:42:17 +0530

    Add code block indent and de-indent selection

--- apps/notes-app/src/components/Editor/extensions/CodeBlock/index.ts
+++ apps/notes-app/src/components/Editor/extensions/CodeBlock/index.ts
@@ -1,6 +1,35 @@
-import { mergeAttributes } from '@tiptap/core';
+import { mergeAttributes, Range } from '@tiptap/core';
 import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight';
 import { codeBlockNodeView } from '@/components/Editor/extensions/CodeBlock/view';
+
+const updateLinesInRange = (
+  code: string,
+  updateLine: (line: string) => string,
+  { from, to }: Range,
+) => {
+  if (from <= 0 || to > code.length) return { code, start: from, end: to };
+
+  const { lines, selStart, selEnd } = code.split('\n').reduce(
+    (acc, line) => {
+      const offset = line.length + acc.offset + 1;
+      const isStartLine = acc.offset <= from && from <= offset;
+      if (from <= offset && to > acc.offset) {
+        const newLine = updateLine(line);
+        const delta = newLine.length - line.length;
+        return {
+          offset,
+          selStart: isStartLine ? acc.selStart + delta : acc.selStart,
+          selEnd: acc.selEnd + delta,
+          lines: [...acc.lines, newLine],
+        };
+      }
+      return { ...acc, offset, lines: [...acc.lines, line] };
+    },
+    { offset: 0, selStart: from, selEnd: to, lines: [] as string[] },
+  );
+
+  return { code: lines.join('\n'), start: selStart, end: selEnd };
+};
 
 export const CodeBlock = CodeBlockLowlight.extend({
   name: 'codeBlock',
@@ -25,30 +54,74 @@ export const CodeBlock = CodeBlockLowlight.extend({
   },
 
   addKeyboardShortcuts() {
+    const INDENT = '  ';
+
     return {
       ...this.parent?.(),
 
       Tab: ({ editor }) => {
         const { selection } = editor.state;
         const { $from, $to } = selection;
+        // If selection start or end is outside block, don't indent
         if ($from.parent.type !== this.type) return false;
+        if ($to.parent.type !== this.type) return false;
 
-        const TAB = '  ';
-
-        console.log(selection.empty);
-        console.log(selection.$from, selection.$to);
-        console.log(editor.state.doc.textBetween($from.pos, $to.pos));
+        // If selection is empty, just insert indent at cursor position
         if (selection.empty) {
           const tr = editor.state.tr;
-          tr.insert(selection.$from.pos, editor.schema.text(TAB));
+          tr.insert($from.pos, editor.schema.text(INDENT));
           editor.view.dispatch(tr);
           return true;
-        } else {
-          // TODO: Insert indent between text
-          // console.log(editor.state.doc.textBetween($from.pos, $to.pos));
         }
 
-        return false;
+        const block = $from.parent;
+        const startPos = selection.$from.start(selection.$from.depth);
+        const indentState = updateLinesInRange(block.textContent, line => INDENT + line, {
+          from: $from.parentOffset,
+          to: $to.parentOffset,
+        });
+
+        return editor
+          .chain()
+          .command(({ tr }) => {
+            tr.replaceWith(
+              startPos,
+              startPos + block.nodeSize,
+              editor.schema.text(indentState.code),
+            );
+            return true;
+          })
+          .setTextSelection({ from: startPos + indentState.start, to: startPos + indentState.end })
+          .run();
+      },
+
+      'Shift-Tab': ({ editor }) => {
+        const { selection } = editor.state;
+        const { $from, $to } = selection;
+        // If selection start or end is outside block, don't de-indent
+        if ($from.parent.type !== this.type) return false;
+        if ($to.parent.type !== this.type) return false;
+
+        const block = $from.parent;
+        const startPos = selection.$from.start(selection.$from.depth);
+        const indentState = updateLinesInRange(
+          block.textContent,
+          line => (line.startsWith(INDENT) ? line.replace(INDENT, '') : line),
+          { from: $from.parentOffset, to: $to.parentOffset },
+        );
+
+        return editor
+          .chain()
+          .command(({ tr }) => {
+            tr.replaceWith(
+              startPos,
+              startPos + block.nodeSize,
+              editor.schema.text(indentState.code),
+            );
+            return true;
+          })
+          .setTextSelection({ from: startPos + indentState.start, to: startPos + indentState.end })
+          .run();
       },
 
       ArrowUp: ({ editor }) => {
